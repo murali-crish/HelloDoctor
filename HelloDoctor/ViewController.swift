8c954a79c19464b7501261848ea31d4b21345317
muralikrishnapatteboyina <murali.patteboyina@ladbrokes.com.au>
2018-02-10 15:45:04 +1000

    Initial Commit

---
+++ HelloDoctor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloDoctor
+//
+//  Created by Murali Krishna Patteboyina on 10/2/18.
+//  Copyright © 2018 Murali Krishna Patteboyina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
